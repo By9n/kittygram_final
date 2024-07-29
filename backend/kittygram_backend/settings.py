58eb6114c7dd85b131b86c86e594aec46933cdda
Александр Буйный <Buapoliv@gmail.com>
2024-07-29 22:08:50 +0300

    fix media-3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -98,8 +98,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/userdata/'
+MEDIA_ROOT = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
